8b8dac89538c97950cd8828ee7de95248817707d
rifqidaiva <rivkidaiva123@gmail.com>
2024-09-22 20:42:42 +0700

    Add drag and drop mechanic

--- internal/pert/node.go
+++ internal/pert/node.go
@@ -1,10 +1,67 @@
 package pert
 
 import (
+	"image/color"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 type node struct {
-	position rl.Vector2
-	radius   int
+	position   rl.Vector2
+	radius     float32
+	color      color.RGBA
+	dragging   bool
+	dragOffset rl.Vector2
+}
+
+func (n *node) update() bool {
+	mousePosition := rl.GetMousePosition()
+
+	if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+		if rl.CheckCollisionPointCircle(mousePosition, n.position, n.radius) {
+			n.dragging = true
+			n.dragOffset = rl.Vector2Subtract(mousePosition, n.position)
+		}
+	}
+
+	if rl.IsMouseButtonReleased(rl.MouseButtonLeft) {
+		n.dragging = false
+	}
+
+	if n.dragging {
+		n.position = rl.Vector2Subtract(mousePosition, n.dragOffset)
+	}
+
+	return n.dragging
+}
+
+func (n node) render() {
+	rl.DrawCircleV(n.position, n.radius, n.color)
+}
+
+func newNode(position rl.Vector2, radius float32) node {
+	return node{
+		position: position,
+		radius:   radius,
+		color: color.RGBA{
+			uint8(rl.GetRandomValue(0, 255)),
+			uint8(rl.GetRandomValue(0, 255)),
+			uint8(rl.GetRandomValue(0, 255)),
+			255,
+		},
+	}
+}
+
+// Function to handle node selection and bring the selected node to the top.
+func handleNodeSelection(nodes *[]node) {
+	mousePosition := rl.GetMousePosition()
+
+	for i := len(*nodes) - 1; i >= 0; i-- {
+		if rl.CheckCollisionPointCircle(mousePosition, (*nodes)[i].position, (*nodes)[i].radius) && rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+			selectedNode := (*nodes)[i]
+			*nodes = append((*nodes)[:i], (*nodes)[i+1:]...)
+			*nodes = append(*nodes, selectedNode)
+			break
+		}
+	}
 }
--- internal/pert/pert.go
+++ internal/pert/pert.go
@@ -15,13 +15,29 @@ func CreatePert(pathListData [][]string) {
 
 	rl.SetTargetFPS(60)
 
+	nodes := []node{
+		newNode(rl.Vector2{X: 100, Y: 100}, 50),
+		newNode(rl.Vector2{X: 300, Y: 100}, 50),
+		newNode(rl.Vector2{X: 500, Y: 100}, 50),
+	}
+
 	for !rl.WindowShouldClose() {
 		handleInputKey()
+		handleNodeSelection(&nodes)
+
+		// Update from top to bottom to find the topmost selected node.
+		for i := len(nodes) - 1; i >= 0; i-- {
+			if dragging := nodes[i].update(); dragging {
+				break
+			}
+		}
+
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
-		rl.DrawText("PERT Diagram", 190, 200, 20, rl.LightGray)
-		rl.DrawCircleV(rl.NewVector2(100, 100), 50, rl.Black)
+		for i := range nodes {
+			nodes[i].render()
+		}
 
 		rl.EndDrawing()
 	}
